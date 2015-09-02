fd3779f1ce8fd968ca7823e0b9c72f0d785c305a
xunchangqing <xunchangqing@qq.com>
2015-09-02 15:16:57 +0800

    use yml to config wecat

--- config/initializers/wechat.rb
+++
@@ -1,3 +0,0 @@
-#require 'yaml'
-#Rails.application.config.wechat = YAML.load_file(Rails.root.join('config', 'wechat.yml'))[Rails.env]
-$wechat_client ||= WeixinAuthorize::Client.new("wx47036a3c431b59a9", "c238bd1564de71932f1e528fb477d044")
