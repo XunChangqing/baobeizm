fe56aaa1f093ba83a7140b6ebf8c4e925f2ae402
xunchangqing <xunchangqing@qq.com>
2015-09-04 15:04:36 +0800

    wechat auth

--- app/controllers/first_price_bargain_controller.rb
+++ app/controllers/first_price_bargain_controller.rb
@@ -1,24 +1,39 @@
 class FirstPriceBargainController < ApplicationController
-  before_action :init_wechat_client
+  before_action :auth_wechat, except: :entrance
   layout 'first_price_bargain'
 
   def entrance
-    Rails.logger.info first_price_bargain_show_url
-    redirect_to $first_wechat_client.authorize_url(first_price_bargain_show_url)
+    sns_info = wechat_client.get_oauth_access_token(params[:code])
+    user_info = wechat_client.get_oauth_userinfo(sns_info.result['openid'], sns_info.result['access_token'])
+    session[:user_info] = user_info.result
+    redirect_to first_price_bargain_show_path
   end
 
   def show
-    sns_info = $first_wechat_client.get_oauth_access_token(params[:code])
-    Rails.logger.info params[:code]
-    #@user_info = $first_wechat_client.get_oauth_userinfo(openid, oauth_token)
-    Rails.logger.info sns_info
+    #Rails.logger.info sns_info.result['openid']
+    #Rails.logger.info sns_info.result['access_token']
+    #user_info = $first_wechat_client.get_oauth_userinfo(sns_info.result['openid'], sns_info.result['access_token'])
+    #Rails.logger.info user_info.inspect
+    @current_user = current_user
   end
+
   def create
   end
 
   private
-  def init_wechat_client
+  def current_user
+    session[:user_info]
+  end
+  def auth_wechat
+    Rails.logger.info session[:user_info].inspect
+    if(session[:user_info] == nil)
+      Rails.logger.info 'redirect to' + wechat_client.authorize_url(first_price_bargain_entrance_url, 'snsapi_userinfo')
+      redirect_to wechat_client.authorize_url(first_price_bargain_entrance_url, 'snsapi_userinfo')
+    end
+  end
+  def wechat_client
     $first_wechat_client ||= WeixinAuthorize::Client.new(Rails.application.config.wechat['first_appid'], Rails.application.config.wechat['first_secret'])
     $first_wechat_client.is_valid?
+    $first_wechat_client
   end
 end
