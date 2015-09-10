aec617fe86dd1f3af845ff1ead39ebe85b4e11a0
xunchangqing <xunchangqing@qq.com>
2015-09-10 23:05:14 +0800

    test

--- app/controllers/first_price_bargain_controller.rb
+++ app/controllers/first_price_bargain_controller.rb
@@ -97,9 +97,9 @@ class FirstPriceBargainController < ApplicationController
         redirect_to wechat_client.authorize_url(request.url, 'snsapi_userinfo')
       else
         sns_info = wechat_client.get_oauth_access_token(params[:code])
-        if sns_info.result['access_token'] == nil
-          redirect_to action: 'show', openid: params[:openid]
-        end
+        #if sns_info.result['access_token'] == nil
+          #redirect_to action: 'show', openid: params[:openid]
+        #end
         user_info = wechat_client.get_oauth_userinfo(sns_info.result['openid'], sns_info.result['access_token'])
         if user_info.result['openid'] == nil
           redirect_to action: 'show', openid: params[:openid]
