23ce1367217794b1792e6651710e1674b9e6650d
xunchangqing <xunchangqing@qq.com>
2015-09-22 17:53:12 +0800

    临时处理排名不一致的情况

--- app/models/first_price_joiner.rb
+++ app/models/first_price_joiner.rb
@@ -9,6 +9,9 @@ class FirstPriceJoiner < ActiveRecord::Base
   def rank
     bigcount = FirstPriceJoiner.where("point > ?", point).count
     equalcount = FirstPriceJoiner.where("point = ? AND updated_at < ?", point, updated_at).count
-    bigcount+equalcount+1
+    #@joiners = FirstPriceJoiner.where(forbidden: [false, nil]).order(point: :desc, updated_at: :asc).page(params[:page])
+    #bigcount+equalcount+1
+    #临时处理由于封号导致个人排名和排名榜不一致的情况
+    bigcount+equalcount
   end
 end
